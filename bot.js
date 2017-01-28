6fadc451c4dabcce5b5b03b2861b54a980147925
Nick Williams <nbransby@gmail.com>
2017-01-28 21:12:40 +0000

    update for dialogue-builder 0.2.0, fixes #4

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
         function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments)).next());
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
 const aws_sdk_1 = require("aws-sdk");
@@ -13,26 +13,31 @@ const onboarding_1 = require("./onboarding");
 const builder = require("claudia-bot-builder");
 module.exports = builder((message, apiRequest) => __awaiter(this, void 0, void 0, function* () {
     try {
-        apiRequest.lambdaContext.callbackWaitsForEmptyEventLoop = true;
         const dynamo = new aws_sdk_1.DynamoDB.DocumentClient();
-        const user = (yield dynamo.get({
-            TableName: 'pointless-bot-users',
-            Key: { fbid: message.sender },
-            ConsistentRead: true
-        }).promise()).Item;
-        const dialogue = new dialogue_builder_1.Dialogue(onboarding_1.default, {
-            retrieve: () => user && user['state'],
-            store: (state) => dynamo.put({
-                TableName: 'pointless-bot-users',
-                Item: { fbid: message.sender, state: state }
-            }, () => { })
-        }, message.sender);
+        const storage = {
+            retrieve() {
+                return __awaiter(this, void 0, void 0, function* () {
+                    const user = yield dynamo.get({
+                        TableName: 'pointless-bot-users',
+                        Key: { fbid: message.sender },
+                        ConsistentRead: true
+                    }).promise();
+                    return user.Item && user.Item['state'];
+                });
+            },
+            store(state) {
+                return __awaiter(this, void 0, void 0, function* () {
+                    yield dynamo.put({
+                        TableName: 'pointless-bot-users',
+                        Item: { fbid: message.sender, state: state }
+                    }).promise();
+                });
+            }
+        };
+        const dialogue = new dialogue_builder_1.Dialogue(onboarding_1.default, storage, message.sender);
         dialogue.setKeywordHandler(['back', 'undo'], 'undo');
         dialogue.setKeywordHandler('reset', 'restart');
-        const messages = dialogue.consume(message);
-        if (dialogue.isComplete) {
-        }
-        return messages;
+        return dialogue.consume(message).catch(() => ['I have said all I have to say']);
     }
     catch (error) {
         console.log(error);
--- bot.ts
+++ bot.ts
@@ -1,38 +1,38 @@
 import { DynamoDB } from 'aws-sdk'
-import { Request } from 'claudia-api-builder';
+import { Request } from 'claudia-api-builder'
 import { Dialogue } from 'dialogue-builder'
 import onboarding from './onboarding'
-import builder = require('claudia-bot-builder');
+import builder = require('claudia-bot-builder')
 import Message = builder.Message
 
 export = builder(async (message: Message, apiRequest: Request) => {
     try {        
-        apiRequest.lambdaContext.callbackWaitsForEmptyEventLoop = true;
-
         const dynamo = new DynamoDB.DocumentClient();
 
-        const user = (await dynamo.get({
-            TableName: 'pointless-bot-users', 
-            Key: { fbid: message.sender }, 
-            ConsistentRead: true 
-        }).promise()).Item;
+        const storage = {
+            async retrieve() {
+                const user = await dynamo.get({
+                    TableName: 'pointless-bot-users', 
+                    Key: { fbid: message.sender }, 
+                    ConsistentRead: true 
+                }).promise();
+                return user.Item && user.Item['state'];
+            },
+            async store(state: Object) { 
+                await dynamo.put({ 
+                    TableName: 'pointless-bot-users', 
+                    Item: { fbid: message.sender, state: state }
+                }).promise();
+            }
+        }
 
-        const dialogue = new Dialogue(onboarding, {
-            retrieve: () => user && user['state'],
-            store: (state: Object) => dynamo.put({ 
-                TableName: 'pointless-bot-users', 
-                Item: { fbid: message.sender, state: state }
-            }, () => {} )
-        }, message.sender);
+        const dialogue = new Dialogue(onboarding, storage, message.sender);
 
         dialogue.setKeywordHandler(['back', 'undo'], 'undo');
         dialogue.setKeywordHandler('reset', 'restart');
 
-        const messages = dialogue.consume(message);
-        if(dialogue.isComplete) {
-            //do something
-        }
-        return messages;
+        return dialogue.consume(message).catch(() => ['I have said all I have to say']);
+
     } catch(error) {
         console.log(error);
         console.log(error.stack);
